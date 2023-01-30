ca77b3dfd2d481e706ad73d00dd9639b7998cc90
yaozu26 <3039770443@qq.com>
2023-01-30 22:33:21 +0800

    搭建TabBar

---
+++ src/assets/data/tab-bar.js
@@ -0,0 +1,28 @@
+const tabBarData = [
+  {
+    name: "首页",
+    image: "tabbar/tab_home.png",
+    imageActive: "tabbar/tab_home_active.png",
+    path: "/home"
+  },
+  {
+    name: "收藏",
+    image: "tabbar/tab_favor.png",
+    imageActive: "tabbar/tab_favor_active.png",
+    path: "/favor"
+  },
+  {
+    name: "订单",
+    image: "tabbar/tab_order.png",
+    imageActive: "tabbar/tab_order_active.png",
+    path: "/order"
+  },
+  {
+    name: "消息",
+    image: "tabbar/tab_message.png",
+    imageActive: "tabbar/tab_message.png",
+    path: "/message"
+  },
+]
+
+export default tabBarData
---
+++ src/components/tab-bar/tab-bar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import tabBarData from '@/assets/data/tab-bar';
+import { getAssetsImg } from "@/utils/load_assets"
+import { ref } from 'vue';
+
+const currentIndex = ref(0)
+</script>
+
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabBarData">
+        <van-tabbar-item :to="item.path">
+          <span>{{ item.name }}</span>
+          <template #icon>
+            <img v-if="currentIndex !== index" :src="getAssetsImg(item.image)" alt="">
+            <img v-else :src="getAssetsImg(item.imageActive)" alt="">
+          </template>
+        </van-tabbar-item>
+      </template>
+    </van-tabbar>
+  </div>
+</template>
+
+<style lang="less" scoped>
+.tab-bar {
+  img {
+    height: 26px;
+  }
+}
+</style>
